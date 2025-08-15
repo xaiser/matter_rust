d2a838e2775ff6f4ab2cc4a37d84e1353d4ddc7d
CY Fan <sis10045>
2025-08-16 07:56:09 +0800

    finish default key name

--- src/chip/chip_lib/support/default_storage_key_allocator.rs
+++ src/chip/chip_lib/support/default_storage_key_allocator.rs
@@ -4,58 +4,141 @@ use core::fmt::{self, Arguments, Write};
 
 struct StorageKeyName{
     m_key_name_buffer: [u8; KKEY_LENGTH_MAX],
-    pos: usize,
+    len: Option<usize>,
+}
+
+impl Default for StorageKeyName {
+    fn default() -> Self {
+        StorageKeyName {
+            m_key_name_buffer: [0; KKEY_LENGTH_MAX],
+            len: None,
+        }
+    }
 }
 
 impl Drop for StorageKeyName {
     fn drop(&mut self) {
         self.m_key_name_buffer = [0; KKEY_LENGTH_MAX];
-        self.pos = 0;
+        self.len = None;
     }
 }
 
 impl StorageKeyName {
-    fn key_name(&self) -> &[u8] {
-        return &self.m_key_name_buffer[..];
+    pub fn key_name(&self) -> &[u8] {
+        if let Some(len) = self.len {
+            return &self.m_key_name_buffer[..len];
+        } else {
+            return &[];
+        }
     }
 
-    fn key_name_raw(&self) -> * const u8 {
-        return self.m_key_name_buffer.as_ptr();
+    pub fn key_name_raw(&self) -> (* const u8, usize) {
+        if let Some(len) = self.len {
+            return (self.m_key_name_buffer.as_ptr(), len);
+        } else {
+            return (core::ptr::null(), 0);
+        }
     }
 
-    fn is_initialized(&self) -> bool {
-        self.m_key_name_buffer[0] != 0
+    pub fn is_initialized(&self) -> bool {
+        self.len.is_some()
     }
 
-    fn is_uninitialized(&self) -> bool {
-        self.m_key_name_buffer[0] == 0
+    pub fn is_uninitialized(&self) -> bool {
+        !self.is_initialized()
+    }
+
+    pub fn formatted(arg: Arguments) -> Self {
+        let mut name = Self::default();
+
+        write!(&mut name, "{}", arg).unwrap();
+
+        return name;
     }
 }
 
 impl From<&[u8]> for StorageKeyName {
     fn from(value: &[u8]) -> Self {
-        let mut name = StorageKeyName {
-            m_key_name_buffer: [0; KKEY_LENGTH_MAX],
-        };
+        let mut name = StorageKeyName::default();
+
         if value.len() == 0 {
-            name.m_key_name_buffer[0] = b'\0';
+            name.len = Some(0);
+            return name;
         }
 
-        let size = core::cmp::min(KKEY_LENGTH_MAX - 1, value.len());
+        let size = core::cmp::min(KKEY_LENGTH_MAX, value.len());
         name.m_key_name_buffer[0..size].copy_from_slice(&value[0..size]);
-        name.m_key_name_buffer[size] = b'\0';
+        name.len = Some(size);
+        return name;
+    }
+}
+
+impl From<&str> for StorageKeyName {
+    fn from(value: &str) -> Self {
+        let mut name = StorageKeyName::default();
+
+        if value.len() == 0 {
+            name.len = Some(0);
+            return name;
+        }
+
+        let size = core::cmp::min(KKEY_LENGTH_MAX, value.len());
+        name.m_key_name_buffer[0..size].copy_from_slice(&value.as_bytes()[0..size]);
+        name.len = Some(size);
         return name;
     }
 }
 
 impl Write for StorageKeyName {
     fn write_str(&mut self, s: &str) -> fmt::Result {
+        let current_len_ref = self.len.get_or_insert(0);
+        let current_len = *current_len_ref;
         let bytes = s.as_bytes();
-        let to_copy = bytes.len().min(KKEY_LENGTH_MAX);
-        self.buf[self.pos..self.pos + to_copy].copy_from_slice(&bytes[..to_copy]);
-        self.pos += to_copy;
-        // break the build to know whwre to start next time.
-        akldjfpoia
+        let to_copy = bytes.len().min(self.m_key_name_buffer.len() - current_len);
+        self.m_key_name_buffer[current_len..current_len + to_copy].copy_from_slice(&bytes[..to_copy]);
+        *current_len_ref += to_copy;
         Ok(())
     }
+}
+
+pub struct DefaultStorageKeyAllocator;
+
+impl DefaultStorageKeyAllocator {
+    pub fn fabric_index_info() -> StorageKeyName {
+        StorageKeyName::from("g/fidx")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    mod storage_key_name {
+        use super::super::*;
+
+        #[test]
+        fn init() {
+            let name = StorageKeyName::default();
+            assert_eq!(true, name.is_uninitialized());
+        }
+
+        #[test]
+        fn write_string() {
+            let name = StorageKeyName::formatted(format_args!("{} is {}", 'a', 'b'));
+            assert_eq!(b"a is b", name.key_name());
+        }
+
+        #[test]
+        fn write_u8_array() {
+            let name = StorageKeyName::from(&b"a is b"[..]);
+            assert_eq!(b"a is b", name.key_name());
+        }
+
+        #[test]
+        fn write_str() {
+            let name = StorageKeyName::from("a is b");
+            assert_eq!(b"a is b", name.key_name());
+        }
+    } // end of storage key name test
+
 }
