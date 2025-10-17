0e259ce645c0ed05212cc52cae6b146b2b69e924
CY Fan <sis10045>
2025-10-17 09:01:37 +0800

    impl ans1

--- src/chip/chip_lib/asn1/asn1.rs
+++ src/chip/chip_lib/asn1/asn1.rs
@@ -3,6 +3,10 @@ pub type Oid = u16;
 #[repr(u16)]
 #[derive(PartialEq, Eq)]
 pub enum Asn1Oid {
+    KoidPubKeyAlgoECPublicKey = 0x0101,
+
+    KoidSigAlgoECDSAWithSHA256 = 0x0201,
+
     KoidAttributeTypeCommonName = 0x0301,
     KoidAttributeTypeSurname = 0x0302,
     KoidAttributeTypeSerialNumber = 0x0303,
@@ -26,6 +30,87 @@ pub enum Asn1Oid {
     KoidAttributeTypeMatterFabricId = 0x0315,
     KoidAttributeTypeMatterCASEAuthTag = 0x0316,
     KoidAttributeTypeMatterVidVerificationSignerId = 0x0317,
+
+    KoidEllipticCurvePrime256v1 = 0x0401,
+
+    KoidExtensionBasicConstraints = 0x0501,
+    KoidExtensionKeyUsage = 0x0502,
+    KoidExtensionExtendedKeyUsage = 0x0503,
+    KoidExtensionSubjectKeyIdentifier = 0x0504,
+    KoidExtensionAuthorityKeyIdentifier = 0x0505,
+    KoidExtensionCSRRequest = 0x0506,
+
+    KoidKeyPurposeServerAuth = 0x0601,
+    KoidKeyPurposeClientAuth = 0x0602,
+    KoidKeyPurposeCodeSigning = 0x0603,
+    KoidKeyPurposeEmailProtection = 0x0604,
+    KoidKeyPurposeTimeStamping = 0x0605,
+    KoidKeyPurposeOCSPSigning = 0x0606,
+
     KoidNotSpecified = 0,
     KoidUnknown = 0xFFFF,
+    KoidEnumMask = 0x00FF
+}
+
+impl From<Asn1Oid> for u16 {
+    fn from(oid: Asn1Oid) -> Self {
+        oid as u16
+    }
+}
+
+impl TryFrom<u16> for Asn1Oid {
+    type Error = ();
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        match value {
+            0x0101 => Ok(Asn1Oid::KoidPubKeyAlgoECPublicKey),
+            0x0201 => Ok(Asn1Oid::KoidSigAlgoECDSAWithSHA256),
+
+            0x0301 => Ok(Asn1Oid::KoidAttributeTypeCommonName),
+            0x0302 => Ok(Asn1Oid::KoidAttributeTypeSurname),
+            0x0303 => Ok(Asn1Oid::KoidAttributeTypeSerialNumber),
+            0x0304 => Ok(Asn1Oid::KoidAttributeTypeCountryName),
+            0x0305 => Ok(Asn1Oid::KoidAttributeTypeLocalityName),
+            0x0306 => Ok(Asn1Oid::KoidAttributeTypeStateOrProvinceName),
+            0x0307 => Ok(Asn1Oid::KoidAttributeTypeOrganizationName),
+            0x0308 => Ok(Asn1Oid::KoidAttributeTypeOrganizationalUnitName),
+            0x0309 => Ok(Asn1Oid::KoidAttributeTypeTitle),
+            0x030A => Ok(Asn1Oid::KoidAttributeTypeName),
+            0x030B => Ok(Asn1Oid::KoidAttributeTypeGivenName),
+            0x030C => Ok(Asn1Oid::KoidAttributeTypeInitials),
+            0x030D => Ok(Asn1Oid::KoidAttributeTypeGenerationQualifier),
+            0x030E => Ok(Asn1Oid::KoidAttributeTypeDNQualifier),
+            0x030F => Ok(Asn1Oid::KoidAttributeTypePseudonym),
+            0x0310 => Ok(Asn1Oid::KoidAttributeTypeDomainComponent),
+            0x0311 => Ok(Asn1Oid::KoidAttributeTypeMatterNodeId),
+            0x0312 => Ok(Asn1Oid::KoidAttributeTypeMatterFirmwareSigningId),
+            0x0313 => Ok(Asn1Oid::KoidAttributeTypeMatterICACId),
+            0x0314 => Ok(Asn1Oid::KoidAttributeTypeMatterRCACId),
+            0x0315 => Ok(Asn1Oid::KoidAttributeTypeMatterFabricId),
+            0x0316 => Ok(Asn1Oid::KoidAttributeTypeMatterCASEAuthTag),
+            0x0317 => Ok(Asn1Oid::KoidAttributeTypeMatterVidVerificationSignerId),
+
+            0x0401 => Ok(Asn1Oid::KoidEllipticCurvePrime256v1),
+
+            0x0501 => Ok(Asn1Oid::KoidExtensionBasicConstraints),
+            0x0502 => Ok(Asn1Oid::KoidExtensionKeyUsage),
+            0x0503 => Ok(Asn1Oid::KoidExtensionExtendedKeyUsage),
+            0x0504 => Ok(Asn1Oid::KoidExtensionSubjectKeyIdentifier),
+            0x0505 => Ok(Asn1Oid::KoidExtensionAuthorityKeyIdentifier),
+            0x0506 => Ok(Asn1Oid::KoidExtensionCSRRequest),
+
+            0x0601 => Ok(Asn1Oid::KoidKeyPurposeServerAuth),
+            0x0602 => Ok(Asn1Oid::KoidKeyPurposeClientAuth),
+            0x0603 => Ok(Asn1Oid::KoidKeyPurposeCodeSigning),
+            0x0604 => Ok(Asn1Oid::KoidKeyPurposeEmailProtection),
+            0x0605 => Ok(Asn1Oid::KoidKeyPurposeTimeStamping),
+            0x0606 => Ok(Asn1Oid::KoidKeyPurposeOCSPSigning),
+
+            0x0000 => Ok(Asn1Oid::KoidNotSpecified),
+            0xFFFF => Ok(Asn1Oid::KoidUnknown),
+            0x00FF => Ok(Asn1Oid::KoidEnumMask),
+
+            _ => Err(()),
+        }
+    }
 }
--- src/chip/credentials/chip_cert.rs
+++ src/chip/credentials/chip_cert.rs
@@ -11,6 +11,7 @@ use crate::chip_ok;
 
 use crate::chip_error_invalid_argument;
 use crate::chip_error_not_implemented;
+use crate::chip_error_no_memory;
 
 use crate::verify_or_return_error;
 use crate::verify_or_return_value;
@@ -21,17 +22,17 @@ pub const K_MAX_RDN_STRING_LENGTH: usize = 10;
 
 #[inline]
 fn is_chip_64bit_dn_attr(oid: Oid) -> bool {
-    return oid == Asn1Oid::KoidAttributeTypeMatterNodeId as u16 ||
-        oid == Asn1Oid::KoidAttributeTypeMatterFirmwareSigningId as u16 ||
-        oid == Asn1Oid::KoidAttributeTypeMatterICACId as u16 ||
-        oid == Asn1Oid::KoidAttributeTypeMatterRCACId as u16 ||
-        oid == Asn1Oid::KoidAttributeTypeMatterFabricId as u16 ||
-        oid == Asn1Oid::KoidAttributeTypeMatterVidVerificationSignerId as u16;
+    return oid == Asn1Oid::KoidAttributeTypeMatterNodeId as Oid ||
+        oid == Asn1Oid::KoidAttributeTypeMatterFirmwareSigningId as Oid ||
+        oid == Asn1Oid::KoidAttributeTypeMatterICACId as Oid ||
+        oid == Asn1Oid::KoidAttributeTypeMatterRCACId as Oid ||
+        oid == Asn1Oid::KoidAttributeTypeMatterFabricId as Oid ||
+        oid == Asn1Oid::KoidAttributeTypeMatterVidVerificationSignerId as Oid;
 }
 
 #[inline]
 fn is_chip_32bit_dn_attr(oid: Oid) -> bool {
-    return oid == Asn1Oid::KoidAttributeTypeMatterCASEAuthTag as u16;
+    return oid == Asn1Oid::KoidAttributeTypeMatterCASEAuthTag as Oid;
 }
 
 #[inline]
@@ -108,7 +109,7 @@ impl ChipRDN {
         Self {
             m_string: [0; K_MAX_RDN_STRING_LENGTH],
             m_chip_val: 0,
-            m_attr_oid: Asn1Oid::KoidNotSpecified as u16,
+            m_attr_oid: Asn1Oid::KoidNotSpecified as Oid,
             m_attr_is_printable_string: false,
         }
     }
@@ -116,12 +117,12 @@ impl ChipRDN {
     pub fn clear(&mut self) {
         self.m_string = [0; K_MAX_RDN_STRING_LENGTH];
         self.m_chip_val = 0;
-        self.m_attr_oid = Asn1Oid::KoidNotSpecified as u16;
+        self.m_attr_oid = Asn1Oid::KoidNotSpecified as Oid;
         self.m_attr_is_printable_string = false;
     }
 
     pub fn is_empty(&self) -> bool {
-        self.m_attr_oid == Asn1Oid::KoidNotSpecified as u16
+        self.m_attr_oid == Asn1Oid::KoidNotSpecified as Oid
     }
 }
 
@@ -133,7 +134,7 @@ impl Default for ChipRDN {
 
 impl PartialEq for ChipRDN {
     fn eq(&self, other: &Self) -> bool {
-        if self.m_attr_oid == Asn1Oid::KoidUnknown as u16 || self.m_attr_oid == Asn1Oid::KoidNotSpecified as u16 || self.m_attr_oid != other.m_attr_oid ||
+        if self.m_attr_oid == Asn1Oid::KoidUnknown as Oid || self.m_attr_oid == Asn1Oid::KoidNotSpecified as Oid || self.m_attr_oid != other.m_attr_oid ||
             self.m_attr_is_printable_string != other.m_attr_is_printable_string {
                 return false;
         }
@@ -160,11 +161,42 @@ impl ChipDN {
     }
 
     pub fn add_attribute(&mut self, oid: Oid, val: u64) -> ChipErrorResult {
-        Err(chip_error_not_implemented!())
+        let rdn_count = self.rdn_count() as usize;
+        verify_or_return_error!(rdn_count < CHIP_CONFIG_CERT_MAX_RDN_ATTRIBUTES, Err(chip_error_no_memory!()));
+        verify_or_return_error!(is_chip_dn_attr(oid), Err(chip_error_invalid_argument!()));
+
+        if is_chip_32bit_dn_attr(oid) {
+            let _ = u32::try_from(val).map_err(|_| chip_error_invalid_argument!())?;
+        }
+
+        self.rdn[rdn_count].m_attr_oid = oid;
+        self.rdn[rdn_count].m_chip_val = val;
+        self.rdn[rdn_count].m_attr_is_printable_string = false;
+
+        chip_ok!()
     }
 
     pub fn add_attribute_printable(&mut self, oid: Oid, val: u64, is_printable_string: bool) -> ChipErrorResult {
-        Err(chip_error_not_implemented!())
+        let rdn_count = self.rdn_count() as usize;
+        verify_or_return_error!(rdn_count < CHIP_CONFIG_CERT_MAX_RDN_ATTRIBUTES, Err(chip_error_no_memory!()));
+        verify_or_return_error!(!is_chip_dn_attr(oid), Err(chip_error_invalid_argument!()));
+        verify_or_return_error!(oid != Asn1Oid::KoidNotSpecified.into(), Err(chip_error_invalid_argument!()));
+
+        self.rdn[rdn_count].m_attr_oid = oid;
+        self.rdn[rdn_count].m_chip_val = val;
+        self.rdn[rdn_count].m_attr_is_printable_string = is_printable_string;
+
+        chip_ok!()
+    }
+
+    pub fn clear(&mut self) {
+        for rdn in &mut self.rdn {
+            rdn.clear();
+        }
+    }
+
+    pub fn rdn_count(&self) -> u8 {
+        self.rdn.iter().take_while(|r| r.is_empty() == false).count() as u8
     }
 }
 
@@ -191,3 +223,113 @@ impl Default for ChipCertificateData {
         ChipCertificateData::const_default()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod rdn {
+        use super::super::*;
+
+        #[test]
+        fn init_chip_rdn() {
+            let rdn = ChipRDN::default();
+            assert_eq!(rdn.m_attr_oid, Asn1Oid::KoidNotSpecified as Oid);
+            assert_eq!(true, rdn.is_empty());
+        }
+
+        #[test]
+        fn empty_oid_is_not_same() {
+            let rdn1 = ChipRDN::default();
+            let rdn2 = ChipRDN::default();
+            assert_eq!(false, rdn1 == rdn2);
+        }
+
+        #[test]
+        fn same_oid() {
+            let mut rdn1 = ChipRDN::default();
+            let mut rdn2 = ChipRDN::default();
+            rdn1.m_attr_oid = Asn1Oid::KoidAttributeTypeMatterNodeId as Oid;
+            rdn2.m_attr_oid = Asn1Oid::KoidAttributeTypeMatterNodeId as Oid;
+            assert_eq!(true, rdn1 == rdn2);
+        }
+
+        #[test]
+        fn different_oid() {
+            let mut rdn1 = ChipRDN::default();
+            let mut rdn2 = ChipRDN::default();
+            rdn1.m_attr_oid = Asn1Oid::KoidAttributeTypeMatterNodeId as Oid;
+            rdn2.m_attr_oid = Asn1Oid::KoidAttributeTypeMatterFabricId as Oid;
+            assert_eq!(false, rdn1 == rdn2);
+        }
+
+        #[test]
+        fn not_matter_oid_same_string() {
+            let mut rdn1 = ChipRDN::default();
+            let mut rdn2 = ChipRDN::default();
+            rdn1.m_attr_oid = Asn1Oid::KoidAttributeTypeCommonName as Oid;
+            rdn2.m_attr_oid = Asn1Oid::KoidAttributeTypeCommonName as Oid;
+            assert_eq!(true, rdn1 == rdn2);
+        }
+
+        #[test]
+        fn not_matter_oid_not_same_string() {
+            let mut rdn1 = ChipRDN::default();
+            let mut rdn2 = ChipRDN::default();
+            rdn1.m_attr_oid = Asn1Oid::KoidAttributeTypeCommonName as Oid;
+            rdn2.m_attr_oid = Asn1Oid::KoidAttributeTypeCommonName as Oid;
+            rdn1.m_string[0] = 0;
+            rdn1.m_string[0] = 1;
+            assert_eq!(false, rdn1 == rdn2);
+        }
+    } // end of rdn
+    
+    mod dn {
+        use super::super::*;
+
+        #[test]
+        fn init() {
+            let dn = ChipDN::default();
+            assert_eq!(0, dn.rdn_count());
+        }
+
+        #[test]
+        fn add_one_attr() {
+            let mut dn = ChipDN::default();
+            
+            assert_eq!(true, dn.add_attribute(Asn1Oid::KoidAttributeTypeMatterNodeId as Oid, 1).is_ok());
+            assert_eq!(1, dn.rdn_count());
+        }
+
+        #[test]
+        fn add_non_matter_oid() {
+            let mut dn = ChipDN::default();
+            
+            assert_eq!(false, dn.add_attribute(Asn1Oid::KoidAttributeTypeCommonName as Oid, 1).is_ok());
+        }
+
+        #[test]
+        fn add_too_much() {
+            let mut dn = ChipDN::default();
+            for i in 0..CHIP_CONFIG_CERT_MAX_RDN_ATTRIBUTES {
+                assert_eq!(true, dn.add_attribute(Asn1Oid::KoidAttributeTypeMatterNodeId.into(), 1).is_ok());
+            }
+            assert_eq!(false, dn.add_attribute(Asn1Oid::KoidAttributeTypeMatterNodeId.into(), 1).is_ok());
+        }
+
+        #[test]
+        fn add_oid_printable() {
+            let mut dn = ChipDN::default();
+            
+            assert_eq!(true, dn.add_attribute_printable(Asn1Oid::KoidAttributeTypeCommonName.into(), 1, true).is_ok());
+        }
+
+        #[test]
+        fn add_oid_printable_not_specified_oid() {
+            let mut dn = ChipDN::default();
+            
+            assert_eq!(false, dn.add_attribute_printable(Asn1Oid::KoidNotSpecified.into(), 1, true).is_ok());
+        }
+    } // end of dn
+
+} // end of tests
